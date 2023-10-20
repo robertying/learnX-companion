94e6da07fe97e0444a30a8a091014d96d6b7ae96
Rui Ying <git@ruiying.io>
2023-10-20 12:24:08 -0700

    Upgrade dependencies

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
@@ -8,17 +7,6 @@ const nextConfig = {
     emotion: true,
   },
   compress: false,
-  modularizeImports: {
-    "@mui/material": {
-      transform: "@mui/material/{{member}}",
-    },
-    "@mui/lab": {
-      transform: "@mui/lab/{{member}}",
-    },
-    "@mui/icons-material": {
-      transform: "@mui/icons-material/{{member}}",
-    },
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = "electron-renderer";
